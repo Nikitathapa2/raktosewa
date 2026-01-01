8e7e493e0a887c5d3389766a009c746af6d17122
Nikitathapa2 <thapanikita456@gmail.com>
2026-01-02 02:03:12 +0545

    create datasource implementation

---
+++ lib/features/auth/data/datasources/organization_local_datasource.dart
@@ -0,0 +1,9 @@
+import '../../domain/entities/organization.dart';
+
+abstract class IOrganizationLocalDataSource {
+  Future<Organization> registerOrganization(Organization organization);
+  Future<Organization> loginOrganization(String email, String password);
+  Future<Organization?> getOrganizationById(String id);
+  Future<bool> updateOrganization(Organization organization);
+  Future<bool> deleteOrganization(String id);
+}
