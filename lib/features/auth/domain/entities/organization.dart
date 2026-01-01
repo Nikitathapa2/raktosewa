320089814af8f9ce2f5fa1249df749443f2c1b0e
Nikitathapa2 <thapanikita456@gmail.com>
2026-01-02 01:46:31 +0545

    create domain entity and repo interface

---
+++ lib/features/auth/domain/entities/organization.dart
@@ -0,0 +1,17 @@
+class Organization {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String address;
+  final String password;
+
+  Organization({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.address,
+    required this.password,
+  });
+}
